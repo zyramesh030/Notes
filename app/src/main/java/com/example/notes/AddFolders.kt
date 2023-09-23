b1935713cb7b6ead441b8ebb3eaef6aed07ffd6b
zyramesh030 <58387787+zyramesh030@users.noreply.github.com>
2023-09-24 01:25:15 +0530

    [Dev] Added activity to add folder.

--- app/src/main/java/com/example/notes/AddFolders.kt
+++ app/src/main/java/com/example/notes/AddFolders.kt
@@ -1,7 +1,12 @@
 package com.example.notes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,6 +14,13 @@ class AddFolders : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_folders)
+        val addFolderBtn: Button = findViewById(R.id.add_folders_btn)
+        addFolderBtn.setOnClickListener(View.OnClickListener {
+            val frTrans: FragmentTransaction = supportFragmentManager.beginTransaction()
+            frTrans.replace(R.id.add_folder_container,
+            NewFolderFragment()).commit()
+
+        })
         val textList: ArrayList<FolderNameModel> =ArrayList()
         val str = "Folder"
 
